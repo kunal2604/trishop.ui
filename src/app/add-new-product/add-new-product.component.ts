4e68f54b34ea391981ddf9be6895bbd5d4467f09
Kunal Kumar <kmrknl2604@gmail.com>
2024-01-27 01:39:01 +0530

    Add new product - form

--- src/app/add-new-product/add-new-product.component.ts
+++ src/app/add-new-product/add-new-product.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, inject } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { Product } from '../_model/product.model';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ProductService } from '../_services/product.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-new-product',
   standalone: true,
-  imports: [MatFormFieldModule, MatButtonModule, MatInputModule ],
+  imports: [MatFormFieldModule, MatButtonModule, MatInputModule, FormsModule ],
   templateUrl: './add-new-product.component.html',
   styleUrl: './add-new-product.component.css'
 })
 export class AddNewProductComponent {
-
+  private _productService = inject(ProductService);
+  product: Product = {
+    productName: "",
+    productDescription: "",
+    price: 0,
+    discount:0
+  }
+  public addProduct(addProductForm: NgForm) {
+    console.log(this.product);
+    this._productService.addNewProduct(this.product).subscribe(
+      (response: Product) => {
+        addProductForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
 }
