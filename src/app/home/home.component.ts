aa3eb590a43fe3ddccc738be48590404340c04e4
Kunal Kumar <kmrknl2604@gmail.com>
2024-02-27 03:15:27 +0530

    Load more button visibility logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { ProductService } from '../_services/product.service';
 import { ImageProcessingService } from '../_services/image-processing.service';
 import { Product } from '../_model/product.model';
 import { map } from 'rxjs';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
@@ -13,7 +13,7 @@ import { ContactUsComponent } from '../shared/components/dialog/contact-us/conta
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule, MatButtonModule, NgFor],
+  imports: [MatGridListModule, MatButtonModule, NgFor, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -24,13 +24,15 @@ export class HomeComponent implements OnInit {
   private _dialogBox = inject(MatDialog);
   productDetails: Product[] = [];
   pageNumber: number = 0;
+  pageSize: number = 4;
+  showLoadButton:boolean = false;
 
   ngOnInit(): void {
    this.getAllProducts();
   }
   
   public getAllProducts() {
-    this._productService.getAllProducts(this.pageNumber, 5)
+    this._productService.getAllProducts(this.pageNumber, this.pageSize)
     .pipe(
       map((x:Product[], i) => x.map((product: Product) => { 
         return this._imageProcessingService.createImagesFromProduct(product);
@@ -39,6 +41,12 @@ export class HomeComponent implements OnInit {
     .subscribe(
       (resp:Product[]) => {
         resp.forEach(p => this.productDetails.push(p));
+        if(resp.length == this.pageSize) {
+          this.showLoadButton = true;
+        }
+        else {
+          this.showLoadButton = false;
+        }
       }
     );
   }
